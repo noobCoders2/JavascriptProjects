28a0c1f3026c15abcbcc2a5570ae8740279e23d7
noobCoder2 <arjaybastillador29@gmail.com>
2026-05-14 06:33:07 +0800

    commit stop watchh and tip calculator

--- todo-app-latest/js/athenticationLogin.js
+++
@@ -1,9 +0,0 @@
-const loginBtn = document.querySelector('.loginBtn');
-loginBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    login();
-});
-
-function login(){
-    console.log(loginBtn.ATTRIBUTE_NODE)
-}
